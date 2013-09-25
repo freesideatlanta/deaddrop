e05ffddcdabdf916eab95070193936076bd5d31d
Alan Fay <alan@faysoftware.com>
2013-09-25 17:44:07 -0400

    including comments about error handling, ConfigParser

--- deaddropnoblock/deaddropdata.py
+++ deaddropnoblock/deaddropdata.py
@@ -6,6 +6,8 @@ class deaddropdata(object):
 		self.block = block
 		import sqlite3 as lite
 		global con
+        # TODO: store the path and name of the database in a config file
+        # TODO: check out ConfigParser: http://docs.python.org/2/library/configparser.html
 		con = lite.connect('deaddrop.db')
 		global cur
 		cur = con.cursor()
@@ -16,6 +18,7 @@ class deaddropdata(object):
 		cur.execute('SELECT MAX(id) FROM files')
 		maxid = cur.fetchone()[0]
 		cur.execute('SELECT * FROM files WHERE id = (?)',(maxid,))
+        # TODO: check for a null or empty result
 		values = cur.fetchall()[0]
 		(offset, size) = values[1], values[2]
 		return (offset, size)
@@ -24,11 +27,14 @@ class deaddropdata(object):
 	def findvalues(self, offset):
 		#find the size of the file matched to the given offset
 		cur.execute('SELECT * FROM files WHERE offset = (?)',(offset,))
+        # TODO: check for a null or empty result
 		size = cur.fetchall()[0][2]
 		return size
 
 
 	def givevalues(self, offset, size):
 		#add the new file's values to the database
+        # TODO: validate the offset and size values before inserting into database
+        # TODO: (make sure they are integers via test)
 		cur.execute('INSERT INTO files(offset, size) VALUES (?,?)',(offset, size))
 		con.commit()
